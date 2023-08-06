8f74881c0f7d06880e0ffb791beebef5bd474495
Niko <t1koni02@students.oamk.fi>
2023-08-06 21:29:38 +0300

    HTML semantics

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        {/* Add more links as needed */}
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
